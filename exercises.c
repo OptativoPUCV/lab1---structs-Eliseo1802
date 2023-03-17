d833270ac6adc04718ef687702c40d1758109bc6
Replit user <>
2023-03-17 18:05:08 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -106,8 +106,7 @@ la cual retorna el valor en la posición i del vector v.
 */
 int obtenerValor(Vector *v, int i)
 {
-  int valor;
-  valor = v->datos[i];
+  int valor = v->datos[i];
   return valor;
 }
 
