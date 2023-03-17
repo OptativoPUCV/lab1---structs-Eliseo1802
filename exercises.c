7ea367269ccd89a2a8d73f3684d3e20b16839deb
Replit user <>
2023-03-17 17:47:45 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -79,16 +79,23 @@ typedef struct {
    int capacidad; // capacidad del arreglo
 } Vector;
 
+/*Vector *crearVector(int n)
+{
+  Vector *v = (Vector*)calloc(n,sizeof(Vector));
+  for(int i = 0;i < n;i++)
+    v->datos[i] = i + 1;
+}*/
+
 Vector *crearVector(int n)
 {
   Vector *v = (Vector*)calloc(n,sizeof(Vector));
   if(v == NULL) return NULL;
-  v->capacidad = n;
+  v.capacidad = n;
   for(int j = 0;j<n;j++)
     for(int i = 0;i<n;i++)
       {
-      v[j].capacidad = n;
-      v[j].datos[i] = 0;
+        v[j].capacidad = n;
+        v[j].datos[i] = 0;
       }
   return v;
 }
